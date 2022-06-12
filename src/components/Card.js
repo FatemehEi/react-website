36851de4cef874143212b5fff5f72adf7aa77b27
FatemehEi <fatemehebrahimi1998@yahoo.com>
2022-06-12 13:10:51 +0430

    add buttons to change number of products

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,31 @@
-import React, { Component } from 'react'
-import styles from "./Card.module.css"
+import React, { Component } from 'react';
+import styles from "./Card.module.css";
+import upArrow from "../images/up-arrow.svg"
+import downArrow from "../images/down-arrow.svg"
 
 export default class Card extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    }
+  }
+
+  downHandler = () => {
+    if(this.state.counter > 0){
+      this.setState(prevState => ({
+        counter: prevState.counter - 1
+      }))
+    }
+  }
+
+  upHandler = () => {
+    this.setState(prevState => ({
+      counter: prevState.counter + 1
+    }))
+  }
+
+
   render() {
     const {image, name, price} = this.props;
     return (
@@ -9,6 +33,12 @@ export default class Card extends Component {
           <img src={image} alt='card'></img>
           <h3>{name}</h3>
           <p>{price}</p>
+          <div className={styles.counter}>
+            <img className={this.state.counter ? "" : styles.deactive} src={downArrow} alt="Down Arrow" onClick={this.downHandler} />
+            <span>{this.state.counter}</span>
+            <img src={upArrow} alt="Up Arrow" onClick={this.upHandler} />
+          </div>
+
       </div>
     )
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,29 @@ import React, { Component } from 'react';
 import styles from './Search.module.css';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: ""
+    }
+  }
+  
+  changeHandler = (event) => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
   render() {
     return (
       <div className={styles.container}>
           <p>Search what you want</p>
-          <input placeholder='Search...' />
+          <div>
+            <input type="text" value={this.state.text} placeholder='Search...' onChange={this.changeHandler} />
+            <br />
+            <br />
+            <span>{this.state.text}</span>
+          </div>
       </div>
     )
   }
